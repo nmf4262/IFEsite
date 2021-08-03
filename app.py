de5a7da7c1d9d9e7b9ed2d01eb6a8e61e18fdd60
Nicolas Ferandez <nmf4262@nyu.edu>
2021-08-03 00:41:20 +0000

    final_trials

--- app.py
+++ app.py
@@ -14,3 +14,8 @@ app = Flask(__name__)
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+@app.route('/superlatives')
+def superlatives():
+    return render_template("superlatives.html")
+
